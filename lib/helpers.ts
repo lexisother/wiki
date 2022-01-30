b30de045dad4abcf99426782ff7a29c7069eee2c
Alyxia Sother <lexisoth2005@gmail.com>
2022-01-30 19:39:55 +0100

    [lib/helpers] Fix some bugs

--- lib/helpers.ts
+++ lib/helpers.ts
@@ -1,24 +1,24 @@
-export function choice(arr: any[]): any[] {
+export function choice(arr: any[]) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-export function shuffle(arr: any[]): any[] {
+export function shuffle(arr: any[]) {
   for (let i = arr.length - 1; i >= 0; i--) {
     let target = Math.floor(Math.random() * (i + 1));
-    [arr[i], arr[target]] = [arr[targer], arr[i]];
+    [arr[i], arr[target]] = [arr[target], arr[i]];
   }
 }
 
 export function floatBetween(a: number, b: number): number {
   if (!a) a = 0;
   if (!b) b = 1;
-  return a + ((b - a) * Math.random());
+  return a + (b - a) * Math.random();
 }
 
 export function randomBinary(length: number): string {
-  let str = ''
-  for (let i = 0; i < length: i++) {
-    str += (Math.round(Math.random())).toString()
+  let str = '';
+  for (let i = 0; i < length; i++) {
+    str += Math.round(Math.random()).toString();
   }
   return str;
 }
