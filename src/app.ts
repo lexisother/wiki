4a96adbde36059abaaa03bb779c5e05a1bd3af0a
Alyxia Sother <lexisoth2005@gmail.com>
2022-01-30 16:36:06 +0100

    [app] Lay the groundwork of the JSON data

--- src/app.ts
+++ src/app.ts
@@ -1,31 +1,63 @@
-import bodyParser from "body-parser";
-import express from "express";
-import { engine } from "express-handlebars";
-import path from "path";
+import bodyParser from 'body-parser';
+import express, { Request, Response, NextFunction } from 'express';
+import { engine } from 'express-handlebars';
+import fs from 'fs/promises';
+import path from 'path';
 
 const PORT = 7004;
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "../public")));
-console.log(path.join(__dirname, "../public"));
-app.engine("hbs", engine());
-app.set("view engine", "hbs");
-
-app.get("/", (req, res) => {
-  return res.render("home", {
-    title: "Home",
-    contohText: "Hello World",
-  });
-});
+app.use(express.static(path.join(__dirname, '../public')));
+console.log(path.join(__dirname, '../public'));
+app.engine('hbs', engine());
+app.set('view engine', 'hbs');
 
 const server = app.listen(
   PORT,
   () => console.log(`Server started on port ${PORT}`), // tslint:disable-line
 );
 
+const jsonColdStorage = ['complete', 'completeKeyed', 'allLinks', 'allTags', 'stats', 'todos'];
+let needsUpdate = false;
+let index = 0;
+jsonColdStorage.forEach(async (variable) => {
+  let filename = `../static/${variable}.json`;
+  await fs
+    .readFile(filename, 'utf8')
+    .then((text) => {
+      /// @ts-expect-error errr, well, yanno... global stuff in TS...
+      global[variable] = JSON.parse(text);
+      console.log(`read ${filename}`);
+    })
+    .catch(async () => {
+      console.log(`couldn't read ${filename} - it will be created`);
+      needsUpdate = true;
+    });
+  if (needsUpdate && index === jsonColdStorage.length - 1) {
+    // TODO: run transpiler
+  }
+  index++;
+});
+
+app.use((request, _response, next) => {
+  console.log(request.url);
+  next();
+});
+
+app.use((_request, response, _next) => {
+  response.status(404).send('404 lmao');
+});
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  if (err) {
+    res.status(500).send('500 lmao: ' + err);
+    console.log(err);
+  }
+});
+
 // Handling terminate gracefully
-process.on("SIGTERM", () => {
+process.on('SIGTERM', () => {
   console.log('SIGTERM signal received.'); // tslint:disable-line
   console.log('Closing Express Server'); // tslint:disable-line
   server.close(() => {
